321ec84ea00528e7e040fd73a95ebba8f51efe1e
a.otcheski <a.otcheski@gmail.com>
2021-06-01 11:15:38 +0700

    Вывод мастей символами

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -3,6 +3,6 @@ values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
 # Создадим массив с набором мастей.
 # Буби, черви, крести, пики.
-suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades']
+suits = ['♦', '♠', '♥', '♣']
 
-puts "#{values.sample} of #{suits.sample}"
+puts values.sample + suits.sample
